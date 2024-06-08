7d1b593f44fb464f93b8fff90259b84b1f0e22b9
Pham Van Thanh <thanhdever@gmail.com>
2024-06-09 00:50:06 +0700

    pvt006 commit

--- booking-hotel-microservice/BookingService/src/main/java/com/microservice/bookingservice/repository/BookingRepository.java
+++ booking-hotel-microservice/BookingService/src/main/java/com/microservice/bookingservice/repository/BookingRepository.java
@@ -3,10 +3,11 @@ package com.microservice.bookingservice.repository;
 import com.microservice.bookingservice.model.BookedRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<BookedRoom, Long> {
     @Query("SELECT b FROM BookedRoom b WHERE b.guestEmail = :email ORDER BY b.bookingId DESC")
-    List<BookedRoom> getBookedRoomsByGuestEmailDESC(String email);
+    List<BookedRoom> getBookedRoomsByGuestEmailDESC(@Param("email") String email);
 }
